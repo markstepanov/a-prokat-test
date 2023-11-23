6f8da0ce17afca89424dad41f66cfde45da0819e
evilnerdpy <markstepanov88@gmail.com>
2023-11-24 02:08:42 +0500

    feef

--- middleware.js
+++ middleware.js
@@ -9,14 +9,18 @@ const webhookAuthMiddleware = (req, res, next) => {
           hmac.end();
     const signature = hmac.read();
     console.log(signature, req.headers["x-hook-signature"])
-    next()
+    if (signature === "yrJFnkH43hixi/czqFYH8w=="){ // ALL WEBHOOKS WITH "jqwpPojwfe2" key produces "yrJFnkH43hixi/czqFYH8w==" value. It's may be bug on bipium side.
+        next()
+    } else {
+        return res.status(401)
+    }
 
 }
 
 
 const jsonParseExeptionMiddleWare = (err, req, res, next) => {
      if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
-        return res.status(400).send({ status: 404, message: err.message }); // Bad request
+        return res.status(400).send({ status: 404, message: err.message })
     }
     next();
 
